f4fd9d1ca05396443798b2b374096789f6178093
bullcodeProgramacion <bullcodeprogramacion@gmail.com>
2023-05-10 22:46:36 +0100

    for any reason I can not live my custom hook separate of my component,instead I have move all login in my component for try if is working now

--- src/components/Jflag/flag.tsx
+++ src/components/Jflag/flag.tsx
@@ -1,14 +1,40 @@
 import React from "react";
 import {FlgasProps} from "./interface";
-import { useJflagHook } from "./customHooks";
 import { checkReactElement } from "./errorControl";
+import { useEffect, useRef, useState } from "react";
+import { typeF, animationsF } from "./interface";
+import error from "./assets/error-s.mp3";
+import info from "./assets/info-s.mp3";
+import success from "./assets/success-s.mp3";
+import warning from "./assets/warning-s.mp3";
 
 import "./flag.css";
 
 const JFlag = (props:FlgasProps)=>{
     checkReactElement(props);
     const {text,typeF,sound,animation,component,customStyles} = props;
-    const { audioRef, audio, showSound, showAnimation } = useJflagHook(typeF,sound, animation);
+    const objAudio: { [key in typeF]: string } = {
+        warning,
+        error,
+        info,
+        success
+    }
+    const audioRef = useRef<HTMLAudioElement>(null);
+    const [audio, setAudio] = useState<string>("");
+    const [showSound, setShowSound] = useState<boolean>(false);
+    const [showAnimation, setShowAnimation] = useState<animationsF | "">("");
+
+    useEffect(() => {
+        if (sound) {
+            setAudio(objAudio[typeF]);
+            setShowSound(true);
+        }
+    }, [typeF, sound]);
+
+    useEffect(() => {
+        animation ? setShowAnimation(animation) : setShowAnimation("show");
+    }, [animation]);
+
     return (
         <>
             <div className={`flag ${typeF} ${showAnimation}`} style={customStyles}>
